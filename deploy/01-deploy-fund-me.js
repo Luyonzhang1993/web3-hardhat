0d7f830a530eabf072a926c4ece0fb211f33138c
Luyon <zhangly@dingdao.com>
2025-09-13 15:17:13 +0800

    feat: import MockV3Aggregator contract

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,10 +1,14 @@
-const { ethers } = require('hardhat');
-
+// const { ethers } = require('hardhat');
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log('this is deploy function.');
     // const [first, second] = await ethers.getSigners();
     // console.log('first', first.address);
     // console.log('second', first.address);
-    const firstAccount = (await getNamedAccounts()).firstAccount;
-    console.log('firstAccount', firstAccount);
+    const { firstAccount } = await getNamedAccounts();
+    const { deploy } = deployments;
+    await deploy("FundMe", {
+        from: firstAccount,
+        args: [10],
+        log: true
+    });
 }
--- test/fundme.test.js
+++ test/fundme.test.js
@@ -1,19 +1,27 @@
-const { ethers } = require('hardhat');
+const { ethers, getNamedAccounts, deployments } = require('hardhat');
 const { assert } = require('chai');
 require('hardhat-deploy');
 
 describe("test fundme contract", async function () {
+    let fundMe;
+    let firstAccount;
+    beforeEach(async function () {
+        firstAccount = (await getNamedAccounts()).firstAccount;
+        const fundMeDeployment = await deployments.get("FundMe");
+        fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address);
+    })
+
     it("test if the owner is msg.sender", async function () {
-        const [firstAccount] = await ethers.getSigners();
-        const fundMeFactory = await ethers.getContractFactory('FundMe');
-        const fundMe = await fundMeFactory.deploy(180);
+        // const [firstAccount] = await ethers.getSigners();
+        // const fundMeFactory = await ethers.getContractFactory('FundMe');
+        // const fundMe = await fundMeFactory.deploy(180);
         await fundMe.waitForDeployment();
-        assert.equal((await fundMe.owner()), firstAccount.address);
+        assert.equal((await fundMe.owner()), firstAccount);
     })
 
     it("test if the dataFeed is right", async function () {
-        const fundMeFactory = await ethers.getContractFactory('FundMe');
-        const fundMe = await fundMeFactory.deploy(180);
+        // const fundMeFactory = await ethers.getContractFactory('FundMe');
+        // const fundMe = await fundMeFactory.deploy(180);
         await fundMe.waitForDeployment();
         assert.equal((await fundMe.dataFeed()), '0x694AA1769357215DE4FAC081bf1f309aDC325306');
     })
